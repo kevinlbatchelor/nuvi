57d1fcc7f96594da12b6cd0c7c923963fcd8604f
kbatchelor <kbatchelor@insidesales.com>
2016-09-22 16:54:45 -0600

    a few tweaks

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # nuvi
+# Best viewed in Chrome :) I didn't add all of the vendor selectors for ie and such
--- main.js
+++ main.js
@@ -1,28 +1,39 @@
 var myApp = angular.module("myApp", []);
 
 myApp.factory("nuviFactory", ['$http', function ($http) {
-    var nuviFactory = {};
+  var nuviFactory = {};
 
-    nuviFactory.getData = function () {
-        return $http({
-            method: 'GET',
-            url: 'https://nuvi-challenge.herokuapp.com/activities'
-        })
-    };
+  nuviFactory.getData = function () {
+    return $http({
+      method: 'GET',
+      url: 'https://nuvi-challenge.herokuapp.com/activities'
+    })
+  };
 
-    return nuviFactory;
+  return nuviFactory;
 }]);
 
 myApp.controller("ctrl", ['$scope', 'nuviFactory',
-    function ($scope, nuviFactory) {
-        var ctrl = this;
-        ctrl.qty = 100;
+  function ($scope, nuviFactory) {
+    var ctrl = this;
+    ctrl.qty = 100;
 
-        nuviFactory.getData().then(function (response) {
-            ctrl.data = _.chunk(response.data, 5);
-        }, function (error) {
-            console.log('shoot things failed ' + error)
-        });
-        $scope.test = 'hi';
-    }]);
+    nuviFactory.getData().then(function (response) {
+      ctrl.data = _.chunk(response.data, 5);
+    }, function (error) {
+      console.log('shoot things failed ' + error)
+    });
+
+    ctrl.like = function (cIndex, pIndex) {
+      ctrl.data.forEach(function (v, i) {
+        if (i === pIndex) {
+          v.forEach(function (child, index) {
+            if (index === cIndex) {
+              child.activity_likes = child.activity_likes + 1;
+            }
+          })
+        }
+      });
+    };
+  }]);
 
